defa84710b9e077a4b8876854d7fcc8788cc216d
Gabriel SS <gabrielsantosgithubac@gmail.com>
2021-12-09 12:53:22 -0300

    Calculator

--- calculator/main.js
+++ calculator/main.js
@@ -7,25 +7,38 @@ function insert(num) {
 };
 function calculate() {
     var number = document.querySelector('#showinc');
+    var showresult = document.querySelector('#showresult').value;
     var values = number.value;
     var array = values.split("");
+    console.log(array);
     if(array[1] == '+') {
         let n1 = Number(array[0]);
         let n2 = Number(array[2]);
-        let convertido1 = parseInt(n1);
-        let convertido2 = parseInt(n2);
         // Calculating:
         let s = n1 + n2;
-        document.querySelector('#showresult').value= s;
-        number.style.color= 'gray';
+        document.querySelector('#showresult').value= `=${s}`;
+        number.style.color= 'rgb(182, 182, 182)';
         number.style.fontSize= 'large';
     }else if(array[1] == 'x') {
        let n1m = Number(array[0]);
        let n2m = Number(array[2]);
-       let convertemulti1 = parseInt(n1m);
-       let convertimulti2 = parseInt(n2m);
        let sm = n1m * n2m;
-       document.querySelector('#showresult').value= sm;
+       document.querySelector('#showresult').value= `=${sm}`;
+       number.style.color= 'rgb(182, 182, 182)';
+       number.style.fontSize= 'large';
+    }else if(array[1] == '÷') {
+        let n1d = Number(array[0]); 
+        let n2d = Number(array[2]);
+        let sd = n1d / n2d;
+        let removefloat = sd.toFixed(2);
+        document.querySelector('#showresult').value= `=${removefloat}`;
+    }else if(array[1] == '-') {
+        let n1s = Number(array[0]);
+        let n2s = Number(array[2]);
+        let ss = n1s - n2s;
+        document.querySelector('#showresult').value= `=${ss}`;
+        number.style.color= 'rgb(182, 182, 182)';
+        number.style.fontSize= 'large';
     }
     
 };
