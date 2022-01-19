d42efae1d2b85d9cc21aa9cf099efef3cd6abf60
Gabriel SS <gabrielsantosgithubac@gmail.com>
2022-01-19 17:26:20 -0300

    calculator

--- calculator/main.js
+++ calculator/main.js
@@ -1,7 +1,8 @@
 function carregar() {
-    document.querySelector('#calculatorbox').style.display='none';
+   /* document.querySelector('#calculatorbox').style.display='none';*/
     var limpar = document.getElementById('clear');
     document.querySelector('#showinc').focus();
+    document.querySelector('#equal').addEventListener('click', calculate)
     limpar.addEventListener('click', cleaninput)
 };
 function insert(num) {
@@ -9,40 +10,45 @@ function insert(num) {
     document.querySelector('#showinc').value= number + num;
 };
 function calculate() {
-    var number = document.querySelector('#showinc');
+    var number = document.querySelector('#showinc').value;
     var showresult = document.querySelector('#showresult').value;
-    var values = number.value;
-    var array = values.split("");
+    var array = number.split("");
+    var pos = [];
+    var todo = [];
+    pos = array.findIndex((item) => {
+        if(item == "+" || item == "-" || item == "x" || item == "÷") {
+            return true
+        }else {
+            return false
+        }
+    })
+    todo = array.find((item) => {
+        if(item == "+") {
+            return addup(array, pos)
+        }else if(item == "-") {
+            return subtraction()
+        }else if(item == "x") {
+            return multiply()
+        }else {
+            return false
+        }
+    })
     console.log(array);
-    if(array[1] == '+') {
-        let n1 = Number(array[0]);
-        let n2 = Number(array[2]);
-        // Calculating:
-        let s = n1 + n2;
-        document.querySelector('#showresult').value= `=${s}`;
-        number.style.color= 'rgb(182, 182, 182)';
-        number.style.fontSize= 'large';
-    }else if(array[1] == 'x') {
-       let n1m = Number(array[0]);
-       let n2m = Number(array[2]);
-       let sm = n1m * n2m;
-       document.querySelector('#showresult').value= `=${sm}`;
-       number.style.color= 'rgb(182, 182, 182)';
-       number.style.fontSize= 'large';
-    }else if(array[1] == '÷') {
-        let n1d = Number(array[0]); 
-        let n2d = Number(array[2]);
-        let sd = n1d / n2d;
-        let removefloat = sd.toFixed(2);
-        document.querySelector('#showresult').value= `=${removefloat}`;
-    }else if(array[1] == '-') {
-        let n1s = Number(array[0]);
-        let n2s = Number(array[2]);
-        let ss = n1s - n2s;
-        document.querySelector('#showresult').value= `=${ss}`;
-        number.style.color= 'rgb(182, 182, 182)';
-        number.style.fontSize= 'large';
-    }
+    console.log(pos);
+};
+function addup(array, pos) {
+ /* array.splice(pos, 1);
+  let nums = array.map((item) => parseInt(item))*/
+ 
+};
+function subtraction() {
+    console.log('subtraction');
+};
+function multiply() {
+    console.log('multiplying');
+};
+function divide() {
+    console.log('divide');
 };
 function cleaninput() {
     document.querySelector('#showinc').value='';
@@ -51,7 +57,7 @@ function cleaninput() {
     document.querySelector('#showinc').style.color='white';
     document.querySelector('#showinc').style.fontSize='xx-large';
 };
-function removemsg() {
+/*function removemsg() {
    document.querySelector('#msg').style.display='none';
    document.querySelector('#calculatorbox').style.display='block';
-};
+};*/
